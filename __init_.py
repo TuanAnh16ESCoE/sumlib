36d6de115298097fa3e6f2d10a331c1f1d4f215c
Tuan Anh <tuananh.tlhht@gmail.com>
2024-01-21 14:33:19 +0700

    first create of sum lib

---
+++ __init_.py
@@ -0,0 +1,2 @@
+# __init__.py
+name = 'test_sum'
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='suumfunction',
+    version='0.1',
+    url='',
+    license='',
+    author='Andrew',
+    author_email='',
+    description='just example sum of 2 number',
+    packages=[''],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
